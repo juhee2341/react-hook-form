a994e8cca297c5fc702de0190c47258dd231f1fc
이주희(누리플렉스) <jhlee96@nuriflex.co.kr>
2023-01-02 17:56:09 +0900

    password 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import Header from "./components/Header";
+
+interface IFormInputs {
+  Email: string;
+  Name: string;
+  Password: string;
+  Password_Confirm: string;
+  Residence: string;
+  Welcome_Message: string;
+}
+
 
 function App() {
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<IFormInputs>();
+  const [data, setData] = useState("");
+  const password = useRef({});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <Header />
+        <label>Email</label>
+        <input
+          {...register('Email', { required: "Email is required.", pattern: { value: /^\S+@\S+$/i, message: 'Check your format' } })}
+          type="email"
+          placeholder="ex) practice@gmail.com"
+        />
+        {errors.Email && <p>{errors.Email.message}</p>}
+        <label>Name</label>
+        <input
+          {...register("Name", { required: "Name is required.", maxLength: { value: 30, message: 'Up to 30 characters.' } })}
+          placeholder="ex) Lee Jay"
+        />
+        {errors.Name && <p>{errors.Name.message}</p>}
+        <label>Password</label>
+        <input
+          {...register("Password", { required: "Password is required.", minLength: { value: 6, message: 'Must be longer than 6 characters.' } })}
+          type="password"
+        />
+        {errors.Password && <p>{errors.Password.message}</p>}
+        <label>Password Confirm</label>
+        <input
+          {...register("Password_Confirm", { required: "Check your password", validate: (value) => value === password.current })}
+          type="password"
+        />
+        {errors.Password_Confirm && <p>{errors.Password_Confirm.message}</p>}
+        {errors.Password_Confirm?.type === 'validate' && <p>The passwords do not match.</p>}
+        <label>Residence</label>
+        <select
+          {...register("Residence", { required: true })}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value="">Please select !!!</option>
+          <option value="Seoul">Seoul</option>
+          <option value="Inchoen">Incheon</option>
+          <option value="Others">Others</option>
+        </select>
+        <label>Welcome Message</label>
+        <textarea
+          {...register("Welcome_Message")}
+          placeholder="Welcome Message"
+        />
+        <p>{data}</p>
+        <input type="submit" />
+      </form>
+    </div >
   );
 }
 
